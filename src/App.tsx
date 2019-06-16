704c1c986a30bb056ac89b35fa6bc5e9d3809b82
ankitpahwa111 <ankitpahwa111@gmail.com>
2019-06-17 02:47:12 +0530

    yo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import { number } from "prop-types";
 
+interface ITodo {
+  text: string;
+  complete: boolean;
+}
 function App() {
   const [value, setValue] = useState<string>("");
+  const [todos, setTodos] = useState<ITodo[]>([]);
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
+    addTodo(value);
     setValue("");
+  };
+  const addTodo = (text: string): void => {
+    const newTodos: ITodo[] = [...todos, { text, complete: false }];
+    setTodos(newTodos);
+  };
+  const completeTodo = (index: number): void => {
+    const newTodos: ITodo[] = [...todos];
+    newTodos[index].complete = !newTodos[index].complete;
+    setTodos(newTodos);
   };
   return (
     <div className="App">
@@ -19,6 +34,16 @@ function App() {
         />
         <button type="submit">Add TODO</button>
       </form>
+      <section>
+        {todos.map((todo: ITodo, index: number) => {
+          return (
+            <Fragment key={index}>
+              <div>{todo.text}</div>
+              <button type="button" onClick={() => completeTodo(index)}>{todo.complete ? 'Incomplete' : 'complete'}</button>
+            </Fragment>
+          );
+        })}
+      </section>
     </div>
   );
 }
